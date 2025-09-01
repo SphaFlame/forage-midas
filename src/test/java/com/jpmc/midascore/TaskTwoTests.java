50bf911087f8268511d1d532c44b2c9a8aa7d57c
Sphamandla <110085668+SphaFlame@users.noreply.github.com>
2025-09-01 12:36:59 +0200

    Update TaskTwoTests.java
    s

--- src/test/java/com/jpmc/midascore/TaskTwoTests.java
+++ src/test/java/com/jpmc/midascore/TaskTwoTests.java
@@ -12,6 +12,7 @@ import org.springframework.test.annotation.DirtiesContext;
 @DirtiesContext
 @EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 class TaskTwoTests {
+
     static final Logger logger = LoggerFactory.getLogger(TaskTwoTests.class);
 
     @Autowired
